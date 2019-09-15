3150c3962b7cdd41dabe9f54ed31945510fac33d
CMDezz <53512796+CMDezz@users.noreply.github.com>
2019-09-15 10:57:44 +0700

    Add files via upload

--- Tuan1/Lab3/Bai4.java
+++ Tuan1/Lab3/Bai4.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Bai4{
     public static void main(String[] args) {
-     
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap so luong sinh vien : ");
-        int size = scanner.nextInt();
-        String[][] arr = new String[size][] ;
-        for (int i = 0 ; i < size ; i ++){
-            String temp = new String[1];
-            System.out.print("Nhap Ten SV : ");
-            String hoTen = scanner.nextLine();
-            temp[0] = hoTen;
-            System.out.print((temp));
-
-            // arr[i] = hoTen;
-            // System.out.print("Nhap diem SV : ");
-            // String diem = scanner.nextLine();
-            // arr[i][1] = diem;
-           
-        // System.out.print(Arrays.toString(arr));
+        int soLuong = scanner.nextInt();
+        String[][] arr = new String[soLuong][2];
+        for (int i = 0 ; i < soLuong ; i++){
+            System.out.print("Nhap ten sinh vien  : ");
+            String hoTen = scanner.next();
+            System.out.print("Nhap diem sinh vien  : ");
+            String diem = scanner.next();
+            arr[i][0] = hoTen;
+            arr[i][1]= diem;
+            
         }
-        
+        sapXep(arr);
+        for (int i = 0 ; i < soLuong ; i ++ ){ //show result
+            Double Diem = Double.parseDouble(arr[i][1]);
+            String hocLuc = ((Diem<5)?"Yeu":(Diem<6.5)?"Trung Binh":(Diem<7.5)?"Kha":(Diem<9)?"Gioi":"Xuat Xac");
+            System.out.println("--- "+Arrays.toString(arr[i])+ " - Hoc Luc : "+hocLuc);
+        }
+    
+
     }
+    public static void sapXep(String[][] arr){
+        String[] temp = new String[2]; 
+        for (int i = 0 ; i <arr.length-1;i++){
+         Double min = Double.parseDouble(arr[i][1]);
+         int iMin= i;
+             for (int j = i+1; j <arr.length;j++){   //tim min
+                 if(Double.parseDouble(arr[j][1])<min) {
+                     min =Double.parseDouble(arr[j][1]);
+                     iMin = j;
+                 }
+              }
+              //chuyen 
+              temp = arr[i] ;
+              arr[i]  = arr[iMin];
+              arr[iMin] = temp;
+        }  
+     }
+   
 }
